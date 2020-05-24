ba5fbb28cc01087593f5af69ae213ef42a4d4654
AlphaMikeNeko <alphamikeneko@gmail.com>
2020-05-24 15:31:37 +0900

    Add a transaction to the block.

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,32 @@
+import hashlib
+import json
+
+from time import time
+from uuid import uuid4
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self):
         # Create a new block and add it to the chain.
-        pass
+        # :param proof: <int>
+        # :param previous_hash: <str>
+        # :return: <dict>
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipinet, amount):
         # Add a new transaction to the list.
@@ -26,9 +47,27 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         # Hash the block.
-        pass
+        # :param block: <dict>
+        # :return: <str>
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     
     @property
     def last_block(self):
         # Return the last block in the chain.
-        pass
+        return self.chain[-1]
+
+    
+    def proof_of_work(self, last_proof):
+        # :param last_proof: <int>
+        # :return: <int>
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
